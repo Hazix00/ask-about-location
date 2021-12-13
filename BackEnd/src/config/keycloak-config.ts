cc99213933942477ad73070aab3397fd96214910
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-12-13 23:57:15 +0100

    configuring keycloak on the backend

---
+++ BackEnd/src/config/keycloak-config.ts
@@ -0,0 +1,25 @@
+import { MemoryStore } from 'express-session'
+import Keycloak from 'keycloak-connect'
+
+let _keycloak: Keycloak.Keycloak;
+
+
+
+export const initKeycloak = (memoryStore: MemoryStore) => {
+    if (_keycloak) {
+        console.warn("Trying to init Keycloak again!");
+        return _keycloak;
+    } 
+    else {
+        console.log("Initializing Keycloak...");
+        _keycloak = new Keycloak({ store: memoryStore });
+        return _keycloak;
+    }
+}
+
+export const getKeycloak = () => {
+    if (!_keycloak){
+        console.error('Keycloak has not been initialized. Please call init first.');
+    } 
+    return _keycloak;
+}
--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -5,13 +5,17 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import session, { MemoryStore } from "express-session";
+
 import { errorHandler } from "./middleware/error.middleware";
 import { notFoundHandler } from "./middleware/not-found.middleware";
+import { initKeycloak } from './config/keycloak-config'
+
 
 dotenv.config();
 /**
  * App Variables
- */
+ */ 
 if (!process.env.PORT){
     process.exit(1);
 }
@@ -26,10 +30,25 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req : Request, res: Response, next:any) => {
-    res.status(200).send('Hello world')
-})
+//keycloak Configuraton
+const memoryStore = new MemoryStore();
+app.use(session({ 
+    secret: 'NFNZQefzsrURjZ88mRYsKUSKUQcfzsWM', 
+    resave: false, 
+    saveUninitialized: true, 
+    store: memoryStore
+ }));
+const keycloak = initKeycloak(memoryStore)
+app.use(keycloak.middleware({logout: '/logout', admin: '/'}))
 
+// Routes
+const testController = require('./controllers/testController').testController
+app.use('/api/test', testController)
+
+app.get('/', (req, res) => {
+    res.send("Server is up!");
+});
+//Error handlers
 app.use(errorHandler);
 app.use(notFoundHandler);
 /**
