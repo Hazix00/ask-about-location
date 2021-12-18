b232399ddabcf4f7c7b1a7365fc02e57d1334773
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-12-18 21:03:02 +0100

    using the generique DTO for the api return type

--- FrontEnd/src/app/services/cities.service.ts
+++ FrontEnd/src/app/services/cities.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ApiModelDTO } from '../dtos/apiModel.dto';
 import { City } from '../models/city.model';
 
 @Injectable({
@@ -11,7 +12,7 @@ export class CitiesService {
   endpoint = environment.apiUrl + '/cities'
   constructor(private readonly http: HttpClient) {}
 
-  get(search:string, match: boolean = false): Observable<City[]> {
-    return this.http.get<City[]>(this.endpoint + `?search=${search}&match=${match}`)
+  get(search:string, match: boolean = false): Observable<ApiModelDTO<City>[]> {
+    return this.http.get<ApiModelDTO<City>[]>(this.endpoint + `?search=${search}&match=${match}`)
   }
 }
