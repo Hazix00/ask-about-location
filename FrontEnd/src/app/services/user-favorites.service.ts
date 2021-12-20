3628e2e4ca858fdad5eb1107a13cdce1f9499501
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-12-20 10:47:52 +0100

    get user favorites service

--- FrontEnd/src/app/services/user-favorites.service.ts
+++ FrontEnd/src/app/services/user-favorites.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ApiModelDTO } from '../dtos/apiModel.dto';
+import { FilteredQuestionDTO } from '../dtos/questions/filteredQuestion.dto';
+import { Favorite } from '../models/favorite.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserFavoritesService {
 
-  constructor() { }
+  private endpoint = environment.apiUrl + '/favorites'
+
+  constructor(private readonly http: HttpClient) { }
+
+  getQuestions(page: number, limit: number) {
+    const qParams = `?page=${page}&limit=${limit}`
+    return this.http.get<ApiModelDTO<FilteredQuestionDTO>[]>(this.endpoint + qParams)
+  }
+
+  getQuestionsIds() {
+    return this.http.get<ApiModelDTO<Favorite>[]>(this.endpoint + '/question-ids')
+  }
 }
